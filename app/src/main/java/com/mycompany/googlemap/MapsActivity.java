76ac66dce2a6681245c702396fffefe1e06eb1a8
Tim <byerst@hawaii.edu>
2015-11-09 15:33:23 -1000

    Added address search bar

--- app/src/main/java/com/mycompany/googlemap/MapsActivity.java
+++ app/src/main/java/com/mycompany/googlemap/MapsActivity.java
@@ -1,11 +1,16 @@
 package com.mycompany.googlemap;
 
 import android.content.Context;
-import android.location.Criteria;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,9 +18,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private float eta = 5; // time to destination
+    private LatLng dest;    // destination latitude and longitude
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,41 @@ public class MapsActivity extends FragmentActivity {
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    public void onSearch(View view) throws IOException {
+
+        Log.d("onSearch","entered");
+        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(location_tf.getWindowToken(), 0);
+        location_tf.setText("");
+        location_tf.clearFocus();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals(" ")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(addressList.get(0) != null) {
+                Address address = addressList.get(0);
+
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+            else{
+                Log.d("error", "addressList[0] == null");
+                return;
+            }
+
+        }
     }
 
     /**
@@ -59,9 +104,6 @@ public class MapsActivity extends FragmentActivity {
     }
 
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
@@ -74,13 +116,15 @@ public class MapsActivity extends FragmentActivity {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         // Create a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
+        //Criteria criteria = new Criteria();
 
         // Get name of best provider
-        String provider = locationManager.getBestProvider(criteria,true);
+        //String provider = locationManager.getBestProvider(criteria,true);
 
         // Get current Location
-        Location myLocation = locationManager.getLastKnownLocation(provider);
+        Location myLocation = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        if(myLocation == null)
+            Log.d("test", "error");
 
         // Set Google map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
