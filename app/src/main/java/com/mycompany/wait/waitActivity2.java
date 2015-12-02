2080486a1b34aa5f7fc696821e1bbb51ac92ba03
Tim <byerst@hawaii.edu>
2015-12-01 21:48:36 -1000

    improved progress meter and button

--- app/src/main/java/com/mycompany/wait/waitActivity2.java
+++ app/src/main/java/com/mycompany/wait/waitActivity2.java
@@ -6,17 +6,15 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.gms.maps.model.LatLng;
 import com.mycompany.googlemap.R;
 
 
@@ -31,13 +29,16 @@ public class waitActivity2 extends FragmentActivity{
     private double destLong;    //destination longitude
 
     private Location currentLoc;  //current Location
+    Location destLoc;   //destination location
+
+    private float distBetween; //distance between start and end points in meters
+    private float distToSend = 1609;  //distance between points when message should be sent (meters)
+                                        //default value of 1 mile
 
 
 
     private String phoneNo; //= new String("8082236901");  //Michele's number
     private String message;
-    TextView elapsedTime;
-    TextView currentValue;
     Chronometer timer;
     ProgressBar progressBar;
     int progressStatus = 0;
@@ -52,18 +53,26 @@ public class waitActivity2 extends FragmentActivity{
 
         // Get values from intent
         Intent intent = getIntent();
+
         phoneNo = intent.getStringExtra("phoneNo");
         message = intent.getStringExtra("message");
         destLat = intent.getDoubleExtra("destLat", 0);
         destLong = intent.getDoubleExtra("destLong", 0);
 
-        // Set up view elements
-        elapsedTime = (TextView) findViewById(R.id.elapsedTime);
-        timer = (Chronometer) findViewById(R.id.chronometer);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        destLoc = new Location(Context.LOCATION_SERVICE);
+
+        Log.d("Test", Double.toString(destLat));
 
         //initialize locationManager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        // Set up view elements
+        timer = (Chronometer) findViewById(R.id.chronometer);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+
 
 
     }
@@ -76,21 +85,35 @@ public class waitActivity2 extends FragmentActivity{
 
     public void onStart(View view){
         // start timer for elapsed time
+        timer.setBase(SystemClock.elapsedRealtime());
         timer.start();
 
+        destLoc.setLongitude(destLong);
+        destLoc.setLatitude(destLat);
+
+        Log.d("Test", destLoc.toString());
+
+        // get current location
+        currentLoc = getCurrent();
+
+        distBetween = destLoc.distanceTo(currentLoc);
+
+        Log.d("Test",Float.toString(distBetween));
+
+        progressBar.setMax(Math.round(distBetween - distToSend));
         // start new thread to handle collecting current location
         new Thread(new Runnable() {
             public void run() {
 
-                // get current location
-                currentLoc = getCurrent();
+
 
                 // while the current location isn't the destination
-                while (currentLoc.getLongitude() != destLong && currentLoc.getLatitude() != destLat ) {
+                while (destLoc.distanceTo(currentLoc)  > distToSend) {
 
 
                     // re-evaluate current location
                     currentLoc = getCurrent();
+                    progressStatus = Math.round(distBetween - destLoc.distanceTo(currentLoc));
 
                     // update view to reflect new location
                     progressBar.post(new Runnable() {
