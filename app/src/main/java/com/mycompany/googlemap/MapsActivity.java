f952fb71977e2de628a2d368381e46947282291a
Tim <byerst@hawaii.edu>
2017-06-07 23:37:12 -1000

    switch to new google maps implementation

--- app/src/main/java/com/mycompany/googlemap/MapsActivity.java
+++ app/src/main/java/com/mycompany/googlemap/MapsActivity.java
@@ -7,6 +7,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,16 +28,19 @@ import com.mycompany.sms.SendSMSActivity;
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    public GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LatLng dest;    // destination latitude and longitude
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //setUpMapIfNeeded();
 
 
     }
@@ -43,70 +48,70 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+        //setUpMapIfNeeded();
     }
 
     public void onSearch(View view) throws IOException {
 
-            // Initialize EditText field
-            EditText location_tf = (EditText) findViewById(R.id.TFaddress);
-            String location = location_tf.getText().toString();
-            Log.d("map", location);
+        // Initialize EditText field
+        EditText location_tf = (EditText) findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        Log.d("map", location);
 
-            if(location.equals("")){
-                Toast.makeText(getApplicationContext(), "Error -- Enter Address First",
-                        Toast.LENGTH_LONG).show();
+        if (location.equals("")) {
+            Toast.makeText(getApplicationContext(), "Error -- Enter Address First",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        // Hide the keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(location_tf.getWindowToken(), 0);
+
+        // Clear edit text field
+        location_tf.setText("");
+        location_tf.clearFocus();
+
+        List<Address> addressList = null;
+
+
+        // if there is a location
+        if (location != null || !location.equals(" ")) {
+            // set up geocoder
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                // add location to addressList
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // if value placed in address list
+            if (addressList.get(0) != null) {
+                Address address = addressList.get(0);
+
+                // Get lat/lng of address
+                dest = new LatLng(address.getLatitude(), address.getLongitude());
+
+                // place a marker at point
+                mMap.addMarker(new MarkerOptions().position(dest).title("Destination"));
+                // adjust camera to marker
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(dest));
+
+            } else {
+                Log.d("error", "addressList[0] == null");
                 return;
             }
 
-
-            // Hide the keyboard
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(location_tf.getWindowToken(), 0);
-
-            // Clear edit text field
-            location_tf.setText("");
-            location_tf.clearFocus();
-
-            List<Address> addressList = null;
-        
-
-            // if there is a location
-            if (location != null || !location.equals(" ")) {
-                // set up geocoder
-                Geocoder geocoder = new Geocoder(this);
-                try {
-                    // add location to addressList
-                    addressList = geocoder.getFromLocationName(location, 1);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                // if value placed in address list
-                if (addressList.get(0) != null) {
-                    Address address = addressList.get(0);
-
-                    // Get lat/lng of address
-                    dest = new LatLng(address.getLatitude(), address.getLongitude());
-
-                    // place a marker at point
-                    mMap.addMarker(new MarkerOptions().position(dest).title("Destination"));
-                    // adjust camera to marker
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(dest));
-
-                } else {
-                    Log.d("error", "addressList[0] == null");
-                    return;
-                }
-
-            }
         }
+    }
 
-    public void onSubmit(View view) throws IOException{
+    public void onSubmit(View view) throws IOException {
 
         // if dest value not set, return
-        if(dest == null) {
+        if (dest == null) {
             Toast.makeText(getApplicationContext(), "Error -- Enter Address First",
                     Toast.LENGTH_LONG).show();
             return;
@@ -122,6 +127,7 @@ public class MapsActivity extends AppCompatActivity {
         finish();
 
     }
+
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
@@ -141,8 +147,7 @@ public class MapsActivity extends AppCompatActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
@@ -169,7 +174,7 @@ public class MapsActivity extends AppCompatActivity {
 
         // Get current Location
         Location myLocation = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
-        if(myLocation == null)
+        if (myLocation == null)
             Log.d("test", "error");
 
         // Set Google map type
@@ -182,7 +187,7 @@ public class MapsActivity extends AppCompatActivity {
         double longitude = myLocation.getLongitude();
 
         // Create latlng object of current location
-        LatLng latLng = new LatLng(latitude,longitude);
+        LatLng latLng = new LatLng(latitude, longitude);
 
         // Show current location in google map
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
@@ -190,5 +195,11 @@ public class MapsActivity extends AppCompatActivity {
         // Zoom in the google map
         mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setUpMap();
     }
 }
